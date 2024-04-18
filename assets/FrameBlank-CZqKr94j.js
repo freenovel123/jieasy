74d6274d949db4d1fd19f4a96db02ee4918bb3ee
steve <7579476@qq.com>
2024-04-18 11:14:10 +0800

    修复部分bug，适配最新的接口

--- assets/FrameBlank-CZqKr94j.js
+++
@@ -1,1 +0,0 @@
-import{f as e,_ as n,c as a,o}from"./index-Dr6s025x.js";const t=e({name:"FrameBlank"});function r(c,s,p,_,f,m){return o(),a("div")}const i=n(t,[["render",r]]);export{i as default};
--- assets/index-DVQN4oNU.js
+++
@@ -1,1 +0,0 @@
-import{U as f}from"./index-Dr6s025x.js";export{f as default};
